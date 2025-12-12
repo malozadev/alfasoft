7acc6dbd65e329bc1e5f8ab3219ea3feb40b2e07
malozadev <carlosmalozade@gmail.com>
2025-12-12 17:12:18 +0100

    fix: add contac.show route

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,7 @@ Route::get('contacts', [ContactController::class, 'index'])->name('contacts.inde
 // Rotas protegidas (auth) para criar, editar, atualizar e apagar
 Route::middleware('auth')->group(function () {
     Route::get('contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+    Route::get('contacts/show', [ContactController::class, 'show'])->name('contacts.show');
     Route::post('contacts', [ContactController::class, 'store'])->name('contacts.store');
     Route::get('contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
     Route::put('contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
