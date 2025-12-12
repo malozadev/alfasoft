811ea3687c5926707792c4b79ade2d76bf3ab708
malozadev <carlosmalozade@gmail.com>
2025-12-12 17:40:41 +0100

    fix:navigation

--- routes/web.php
+++ routes/web.php
@@ -30,11 +30,13 @@ Route::middleware('auth')->group(function () {
 });
 
 
+
+
 Route::get('contacts', [ContactController::class, 'index'])->name('contacts.index');
 
 Route::middleware('auth')->group(function () {
     Route::get('contacts/create', [ContactController::class, 'create'])->name('contacts.create');
-    Route::get('contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
+    Route::get('contacts/show', [ContactController::class, 'show'])->name('contacts.show');
     Route::post('contacts', [ContactController::class, 'store'])->name('contacts.store');
     Route::get('contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
     Route::put('contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
