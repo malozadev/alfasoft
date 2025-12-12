4cde1c832ac388d05b649fbee12a4ee913fad06d
malozadev <carlosmalozade@gmail.com>
2025-12-12 18:38:01 +0100

    fix: routes auth

--- routes/auth.php
+++ routes/auth.php
@@ -15,6 +15,8 @@ Route::middleware('guest')->group(function () {
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
+
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
 });
 
 
